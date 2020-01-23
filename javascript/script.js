e3da753ac333a5ceb2a6c09bafb89605cc2a5b6a
Mattia Pedone <mattiapedone93@gmail.com>
2020-01-23 22:58:52 +0100

    EUREKA! il messaggio si invia! :D

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,20 @@
 $(document).ready(function() {
-  
+  $('#add-message').keypress(
+      function (event) {
+        if(event.which == 13 || event.keyCode == 13) {
+          var text = $('#add-message').val();
+          var elementNew = $('.template li').clone();
+          elementNew.append(text);
+          $('.my_message').append(elementNew);
+          $('#add-message').val(' ');
+        }
+      }
+    );
+    $(document).on('click', '.send_icon i', function() {
+      var text = $('#add-message').val();
+      var elementNew = $('.template li').clone();
+      elementNew.append(text);
+      $('.my_message').append(elementNew);
+      $('#add-message').val(' ');
+    })
 });
